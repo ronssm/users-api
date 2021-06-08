bd18b4e967ab8be3fb73312b99bc59815e76e42e
ronaldo_moco <ronaldo.moco@stoneage.com.br>
2021-06-08 16:14:51 -0300

    feat: copying from tutorial

---
+++ app.ts
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv';
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
+
+import express from 'express';
+import * as http from 'http';
+import * as bodyparser from 'body-parser';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { UsersRoutes } from './users/users.routes.config';
+import { AuthRoutes } from './auth/auth.routes.config';
+import debug from 'debug';
+import helmet from 'helmet';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+
+app.use(bodyparser.json());
+app.use(cors());
+app.use(helmet());
+
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false; // when not debugging, make terse
+  if (typeof global.it === 'function') {
+    loggerOptions.level = 'http'; // for non-debug test runs, squelch entirely
+  }
+}
+
+app.use(expressWinston.logger(loggerOptions));
+
+routes.push(new UsersRoutes(app));
+routes.push(new AuthRoutes(app));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send(`Server running at http://localhost:${port}`);
+});
+export default server.listen(port, () => {
+  debugLog(`Server running at http://localhost:${port}`);
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+});
